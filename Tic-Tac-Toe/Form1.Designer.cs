9da61748cbd779ea5ef447856432941eb2acf2fa
VejayDrepaul <vejayddrepaul@gmail.com>
2022-01-05 21:55:51 -0500

    Add project files.

---
+++ Tic-Tac-Toe/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.new_game = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(9, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 131);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_logic);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(140, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 131);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_logic);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(271, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 131);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_logic);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(9, 149);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 131);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_logic);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(140, 149);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(125, 131);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_logic);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(271, 149);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(125, 131);
+            this.button6.TabIndex = 3;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button_logic);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(9, 286);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(125, 131);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button_logic);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(140, 286);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(125, 131);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button_logic);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(271, 286);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(125, 131);
+            this.button9.TabIndex = 6;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button_logic);
+            // 
+            // new_game
+            // 
+            this.new_game.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.new_game.Location = new System.Drawing.Point(9, 423);
+            this.new_game.Name = "new_game";
+            this.new_game.Size = new System.Drawing.Size(387, 53);
+            this.new_game.TabIndex = 9;
+            this.new_game.Text = "New Game";
+            this.new_game.UseVisualStyleBackColor = true;
+            this.new_game.Click += new System.EventHandler(this.new_game_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 482);
+            this.Controls.Add(this.new_game);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button new_game;
+    }
+}
+
---
+++ Tic-Tac-Toe/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<int> options = new List<int>()
+        {
+            1, 2, 3, 4, 5, 6, 7, 8, 9
+        };
+
+        void enabled_false()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            button9.Enabled = false;
+        }
+
+        void computer_guess()
+        {
+            Random random = new Random();
+            int test = random.Next(options.Count);  
+            
+
+            if (test == 1 && button1.Text == "")
+            {
+                button1.Text = "O";
+            }
+
+            if (test == 2 && button2.Text == "")
+            {
+                button2.Text = "O";
+            }
+
+            if (test == 3 && button3.Text == "")
+            {
+                button3.Text = "O";
+            }
+
+            if (test == 4 && button4.Text == "")
+            {
+                button4.Text = "O";
+            }
+
+            if (test == 5 && button5.Text == "")
+            {
+                button5.Text = "O";
+            }
+
+            if (test == 6 && button6.Text == "")
+            {
+                button6.Text = "O";
+            }
+
+            if (test == 7 && button7.Text == "")
+            {
+                button7.Text = "O";
+            }
+
+            if (test == 8 && button8.Text == "")
+            {
+                button8.Text = "O";
+            }
+
+            if (test == 9 && button9.Text == "")
+            {
+                button9.Text = "O";
+            }
+
+            options.Remove(test);
+        }
+
+        void button_logic(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (button.Text == "")
+            {
+                button.Text = "X";
+                button.Enabled = false;
+                computer_guess();
+                score();
+            }
+            else if (button.Text == "O")
+            {
+                MessageBox.Show("This spot is taken!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void new_game_Click(object sender, EventArgs e)
+        {
+            button1.Text = "";
+            button1.BackColor = Color.Transparent;
+
+            button2.Text = "";
+            button2.BackColor = Color.Transparent;
+
+            button3.Text = "";
+            button3.BackColor = Color.Transparent;
+
+            button4.Text = "";
+            button4.BackColor = Color.Transparent;
+
+            button5.Text = "";
+            button5.BackColor = Color.Transparent;
+
+            button6.Text = "";
+            button6.BackColor = Color.Transparent;
+
+            button7.Text = "";
+            button7.BackColor = Color.Transparent;
+
+            button8.Text = "";
+            button8.BackColor = Color.Transparent;
+
+            button9.Text = "";
+            button9.BackColor = Color.Transparent;
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+        }
+
+        void score()
+        {
+            // Horizontal Wins
+            if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X")
+            {
+                button1.BackColor = Color.Green;
+                button2.BackColor = Color.Green;
+                button3.BackColor = Color.Green;
+                MessageBox.Show("You Won!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                enabled_false();
+            }
+
+            if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X")
+            {
+                button4.BackColor = Color.Green;
+                button5.BackColor = Color.Green;
+                button6.BackColor = Color.Green;
+                MessageBox.Show("You Won!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                enabled_false();
+            }
+
+            if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X")
+            {
+                button7.BackColor = Color.Green;
+                button8.BackColor = Color.Green;
+                button9.BackColor = Color.Green;
+                MessageBox.Show("You Won!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                enabled_false();
+            }
+
+            // Vertical Wins
+            if (button1.Text == "X" && button4.Text == "X" && button7.Text == "X")
+            {
+                button1.BackColor = Color.Green;
+                button4.BackColor = Color.Green;
+                button7.BackColor = Color.Green;
+                MessageBox.Show("You Won!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            if (button2.Text == "X" && button5.Text == "X" && button8.Text == "X")
+            {
+                button2.BackColor = Color.Green;
+                button5.BackColor = Color.Green;
+                button8.BackColor = Color.Green;
+                MessageBox.Show("You Won!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+            if (button3.Text == "X" && button6.Text == "X" && button9.Text == "X")
+            {
+                button3.BackColor = Color.Green;
+                button6.BackColor = Color.Green;
+                button9.BackColor = Color.Green;
+                MessageBox.Show("You Won!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            // Diagonal Wins
+            if (button1.Text == "X" && button5.Text == "X" && button9.Text == "X")
+            {
+                button1.BackColor = Color.Green;
+                button5.BackColor = Color.Green;
+                button9.BackColor = Color.Green;
+                MessageBox.Show("You Won!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            if (button3.Text == "X" && button5.Text == "X" && button7.Text == "X")
+            {
+                button3.BackColor = Color.Green;
+                button5.BackColor = Color.Green;
+                button7.BackColor = Color.Green;
+                MessageBox.Show("You Won!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            // Horizontal Wins
+            if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O")
+            {
+                button1.BackColor = Color.Green;
+                button2.BackColor = Color.Green;
+                button3.BackColor = Color.Green;
+                MessageBox.Show("You Won!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                enabled_false();
+            }
+
+            if (button4.Text == "O" && button5.Text == "O" && button6.Text == "O")
+            {
+                button4.BackColor = Color.Green;
+                button5.BackColor = Color.Green;
+                button6.BackColor = Color.Green;
+                MessageBox.Show("You Won!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                enabled_false();
+            }
+
+            if (button7.Text == "O" && button8.Text == "O" && button9.Text == "O")
+            {
+                button7.BackColor = Color.Green;
+                button8.BackColor = Color.Green;
+                button9.BackColor = Color.Green;
+                MessageBox.Show("You Won!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                enabled_false();
+            }
+
+            // Vertical Wins
+            if (button1.Text == "O" && button4.Text == "O" && button7.Text == "O")
+            {
+                button1.BackColor = Color.Green;
+                button4.BackColor = Color.Green;
+                button7.BackColor = Color.Green;
+                MessageBox.Show("You Won!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            if (button2.Text == "O" && button5.Text == "O" && button8.Text == "O")
+            {
+                button2.BackColor = Color.Green;
+                button5.BackColor = Color.Green;
+                button8.BackColor = Color.Green;
+                MessageBox.Show("You Won!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+            if (button3.Text == "O" && button6.Text == "O" && button9.Text == "O")
+            {
+                button3.BackColor = Color.Green;
+                button6.BackColor = Color.Green;
+                button9.BackColor = Color.Green;
+                MessageBox.Show("You Won!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            // Diagonal Wins
+            if (button1.Text == "O" && button5.Text == "O" && button9.Text == "O")
+            {
+                button1.BackColor = Color.Green;
+                button5.BackColor = Color.Green;
+                button9.BackColor = Color.Green;
+                MessageBox.Show("You Won!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            if (button3.Text == "O" && button5.Text == "O" && button7.Text == "O")
+            {
+                button3.BackColor = Color.Green;
+                button5.BackColor = Color.Green;
+                button7.BackColor = Color.Green;
+                MessageBox.Show("You Won!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
